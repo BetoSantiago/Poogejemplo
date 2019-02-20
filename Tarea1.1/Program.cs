c729b2c8a952db0035f44ff7edaeee8dd6e36b20
BetoSantiago <roberto.lira18@tectijuana.edu.mx>
2019-02-20 10:38:53 -0800

    cambios 2
    descripcion 2

--- Tarea1.1/Program.cs
+++ Tarea1.1/Program.cs
@@ -6,7 +6,7 @@ namespace Tarea1._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo!");
+            Console.WriteLine("Hola mun!");
         }
     }
 }
